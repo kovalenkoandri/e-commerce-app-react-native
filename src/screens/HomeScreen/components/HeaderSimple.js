cb20bba1abd5d1816eb9420a463c6e543133e712
kovalenkoandri <kovalenkoandrii@gmail.com>
2023-11-17 18:15:26 +0200

    Keyboard.dismiss TouchableWithoutFeedback

--- src/screens/HomeScreen/components/HeaderSimple.js
+++ src/screens/HomeScreen/components/HeaderSimple.js
@@ -7,13 +7,17 @@ import {
   Image,
   FlatList,
   Platform,
+  Keyboard,
   // TextInput,
 } from "react-native";
 import Colors from "../../../utils/Colors";
 import SearchItem from "./SearchItem";
 // import { TextInput } from 'react-native-paper';
-import { TextInput } from 'react-native-gesture-handler';
-const { width, height } = Dimensions.get("window");
+import {
+  TextInput,
+  TouchableWithoutFeedback,
+} from "react-native-gesture-handler";
+const { height } = Dimensions.get("window");
 
 export class Header extends React.Component {
   constructor(props) {
@@ -59,63 +63,63 @@ export class Header extends React.Component {
             style={styles.input}
           />
         </View>
-        <View style={[styles.content]}>
-          <View style={styles.content_safe_area}>
-            {this.state.keyword === "" ? (
-              <View style={styles.image_placeholder_container}>
-                <Image
-                  source={require("../../../assets/Images/logo1.png")}
-                  style={styles.image_placeholder}
-                />
+        <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+          {this.state.keyword === "" ? (
+            <View style={styles.image_placeholder_container}>
+              <Image
+                source={require("../../../assets/Images/logo1.png")}
+                style={styles.image_placeholder}
+              />
+              <Text style={styles.image_placeholder_text}>
+                Enter keywords{"\n"}
+                to search for :D
+              </Text>
+            </View>
+          ) : (
+            <View
+              style={{
+                marginHorizontal: 20,
+                marginTop:
+                  Platform.OS === "android" ? 0 : height < 668 ? 0 : 60,
+              }}
+            >
+              {this.state.productsFilter.length === 0 ? (
                 <Text style={styles.image_placeholder_text}>
-                  Enter keywords{"\n"}
-                  to search for :D
+                  Product not found
                 </Text>
-              </View>
-            ) : (
-              <View
-                style={{
-                  marginHorizontal: 20,
-                  marginTop:
-                    Platform.OS === "android" ? 0 : height < 668 ? 0 : 60,
-                }}
-              >
-                {this.state.productsFilter.length === 0 ? (
-                  <Text style={styles.image_placeholder_text}>
-                    Product not found
-                  </Text>
-                ) : (
-                  <FlatList
-                    data={this.state.productsFilter}
-                    keyExtractor={(item) => item._id}
-                    renderItem={({ item }) => {
-                      return (
-                        <SearchItem
-                          item={item}
-                          navigation={this.props.navigation}
-                        />
-                      );
-                    }}
-                  />
-                )}
-              </View>
-            )}
-          </View>
-        </View>
+              ) : (
+                <FlatList
+                  style={[styles.flatList]}
+                  data={this.state.productsFilter}
+                  keyExtractor={(item) => item._id}
+                  renderItem={({ item }) => {
+                    return (
+                      <SearchItem
+                        item={item}
+                        navigation={this.props.navigation}
+                      />
+                    );
+                  }}
+                />
+              )}
+            </View>
+          )}
+        </TouchableWithoutFeedback>
       </>
     );
   }
 }
 
 const styles = StyleSheet.create({
+  flatList: {
+    height: height,
+  },
   input_box: {
     height: 50,
     flexDirection: "row",
     alignItems: "center",
     backgroundColor: Colors.bluegreen,
-    width: width,
   },
-
   input: {
     flex: 1,
     height: 40,
@@ -125,17 +129,10 @@ const styles = StyleSheet.create({
     fontSize: 15,
     marginHorizontal: 20,
   },
-  content: {
-    // width: width, // if on => hide Home Screen FlatList
-    // height: height, // if on => hide Home Screen FlatList
-  },
-  content_safe_area: {
-    flex: 1,
-    backgroundColor: Colors.bluegreen, // hide content_safe_area
-  },
   image_placeholder_container: {
     flexDirection: "column",
     marginTop: 100,
+    height: height,
   },
   image_placeholder: {
     height: 80,
@@ -146,5 +143,6 @@ const styles = StyleSheet.create({
     textAlign: "center",
     color: "gray",
     marginTop: 5,
+    height: height,
   },
 });
